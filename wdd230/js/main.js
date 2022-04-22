bbc10a10652eea629cd2dc09061cd661c9310b58
codermott <ode16003@byui.edu>
2022-04-22 12:38:45 -0600

    Update

--- wdd230/js/main.js
+++ wdd230/js/main.js
@@ -12,4 +12,36 @@ let newpara = document.createElement('p');
 newpara.textContent = 'This is my paragraph';
 document.querySelector('div').appendChild(newpara);
 
-document.querySelector('img').setAttribute('src', );
+document.querySelector('img').setAttribute('src', "images/profilepic.png");
+
+document.querySelector('img').setAttribute('width', "400");
+
+document.querySelector("img").classList
+
+// Selection
+
+// if clause
+if (year == 2022) {
+    console.log("current year")
+}
+
+//repetition (loops)
+
+const myarray = ["CSE121b", "WDD230", "CIT 111"];
+
+function makelist(item) {
+    document.querySelector("div").innerHTML += "<li>" + item + "</li>"
+}
+
+myarray.forEach(makelist);
+
+// All I need for js in week 01
+
+// last modified property that works with doc
+
+let currentdate = document.lastModified;
+
+console.log(currentdate);
+document.getElementById("LastMod").innerHTML = currentdate;
+
+console.log(currentdate);
